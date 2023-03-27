246d480e1f531c66a7469fa3d3f4b9b3d2709d67
the-spatializer <74182913+the-spatializer@users.noreply.github.com>
2023-03-27 07:55:37 +0700

    refined date

--- client/src/pages/OrderList.jsx
+++ client/src/pages/OrderList.jsx
@@ -134,8 +134,8 @@ function OrderList(props) {
                     <td>{index+1}</td>
                     <td>{props.Profiles.find(profile => profile.id === order.vendor_id).name}</td>
                     <td onClick={() => handleDetailOrderModal(order.title, order.description, order.price)} className="tw-cursor-pointer !tw-text-custom-primary tw-underline">{order.title}</td>
-                    <td>{order.start_date}</td>
-                    <td>{order.end_date}</td>
+                    <td>{order.start_date.split("T")[0]}</td>
+                    <td>{order.end_date.split("T")[0]}</td>
                     <td className={
                       order.status === "waiting" ? "!tw-text-custom-warning" : 
                       order.status === "success" ? "!tw-text-custom-success" : 
