12c3cb16c0919a034211622e0e061f545beb1029
Fauzan | Fullstack Developer <mfauzan.murtadho@gmail.com>
2024-06-05 21:36:30 +0700

    change api

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const API = axios.create({
-  baseURL: "https://fauzancodes.site/waysgallery/api/v1/",
+  baseURL: "https://api-waysgallery.vercel.app/api/v1/",
 });
 
 export const setAuthToken = (token) => {
