8e76f795709c3ac764a17611e7d1be730faf0bef
Fauzan | Fullstack Developer <mfauzan.murtadho@gmail.com>
2024-06-09 13:56:29 +0700

    some adjustment for vercel

--- server/main.go
+++ server/main.go
@@ -1,25 +1,35 @@
-package main
+package handler
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 	"waysgallery/database"
 	"waysgallery/pkg/postgresql"
 	"waysgallery/routes"
 
-	"github.com/joho/godotenv"
+	_ "github.com/joho/godotenv/autoload"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-	errEnv := godotenv.Load()
-	if errEnv != nil {
-		panic("Failed to load env file")
-	}
+	e := Start()
 
+	var port = os.Getenv("PORT")
+
+	fmt.Println("server running localhost:" + port)
+	e.Logger.Fatal(e.Start(":" + port))
+}
+
+func Main(w http.ResponseWriter, r *http.Request) {
+	e := Start()
+
+	e.ServeHTTP(w, r)
+}
+
+func Start() *echo.Echo {
 	e := echo.New()
-
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
 		AllowMethods: []string{echo.GET, echo.POST, echo.PATCH, echo.DELETE},
@@ -33,8 +43,5 @@ func main() {
 
 	e.Static("/uploads", "./uploads")
 
-	var port = os.Getenv("PORT")
-
-	fmt.Println("server running localhost:" + port)
-	e.Logger.Fatal(e.Start(":" + port))
+	return e
 }
